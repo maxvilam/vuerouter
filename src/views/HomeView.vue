4b1e1010f54058142279114691143c454eda7db8
MaxAvila <maxvilam@hotmail.con>
2022-05-06 21:48:39 -0400

    ok

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Bienvenido a VueRouter y Firebase" />
   </div>
 </template>
 
